98a860e294d967de867c7b3aa7f8b5f20c188604
WallaceClark <walclark@gmail.com>
2022-03-02 21:15:56 -0300

    Configurando arquivos de ambiente e testando a busca

--- exemplo.php
+++ exemplo.php
@@ -1,5 +1,12 @@
 <?php
 
-    echo "Teste";
+    require_once "vendor/autoload.php";
+
+    use WallaceClark\DioCep\Search;
+
+    $busca = new Search();
+    $resultado = $busca->getAddressFromZipcode('04318-000');
+
+    print_r($resultado);
 
 ?>
---
+++ src/Search.php
@@ -0,0 +1,21 @@
+<?php
+
+    namespace WallaceClark\DioCep;
+
+    class Search {
+
+        private $url = "https://viacep.com.br/ws/";
+
+        public function getAddressFromZipcode(string $zipCode):array {
+
+            $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
+            
+            $get = file_get_contents($this->url . $zipCode . "/json");
+
+            return json_decode($get, true);
+
+        }
+
+    }
+
+?>
