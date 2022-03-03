077310d89592756ad8ab4caf0066cd0f7cbc250a
WallaceClark <walclark@gmail.com>
2022-03-02 21:23:38 -0300

    Buscando CEP na API

--- exemplo.php
+++ exemplo.php
@@ -5,7 +5,7 @@
     use WallaceClark\DioCep\Search;
 
     $busca = new Search();
-    $resultado = $busca->getAddressFromZipcode('04318-000');
+    $resultado = $busca->getAddressFromZipcode('01001-000');
 
     print_r($resultado);
 
